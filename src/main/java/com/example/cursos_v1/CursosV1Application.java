9d345c45e64a3d81fda5b7aeffc9aa9f3b249663
dewardh98-sys <dewardh98@gmail.com>
2025-12-29 12:10:54 -0500

    mi repositorio

---
+++ src/main/java/com/example/cursos_v1/CursosV1Application.java
@@ -0,0 +1,13 @@
+package com.example.cursos_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursosV1Application.class, args);
+    }
+
+}
